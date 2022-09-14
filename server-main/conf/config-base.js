de15e7ff6fd1f3dc89864f9a2c4c11cc47b8ab39
Reiko Randoja <reiko.randoja@gmail.com>
2022-09-14 22:22:35 +0300

    Create server to handle root domain and subdomains

---
+++ server-main/conf/config-base.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+
+let config = {};
+
+config.host = 'localhost';
+config.port = 8111;
+config.useHttps = false;
+config.httpsOptions = {};
+
+config.db = {
+    vendor: 'postgres',
+    name: 'pwir',
+    host: 'localhost',
+    port: '5432',
+    username: '',
+    password: '',
+    pool: {
+        min: 0,
+        max: 5
+    }
+};
+
+config.db.connectionParams = {
+    user: config.db.username,
+    database: config.db.name,
+    password: config.db.password,
+    port: config.db.port,
+    host: config.db.host,
+    dateStrings: true
+};
+
+config.db.connectionParamsPrivate = {
+    user: config.db.privateUsername,
+    database: config.db.name,
+    password: config.db.privatePassword,
+    port: config.db.port,
+    host: config.db.host,
+    dateStrings: true
+};
+
+config.db.pgDumpPath = '';
+config.db.adminUsername = '';
+config.db.adminPassword = '';
+
+config.session = {};
+config.session.secret = '';
+
+config.mailgun = {
+    apiKey: '',
+    apiUrl: '',
+    domain: '',
+    from: '',
+    loginLinkHostname: '',
+};
+
+module.exports = config;
---
+++ server-main/server-main.mjs
@@ -0,0 +1,51 @@
+import config from './conf/config.js';
+import {createProxyMiddleware} from 'http-proxy-middleware';
+import helmet from 'helmet';
+import https from 'https';
+import http from 'http';
+import express from 'express';
+import vhost from 'vhost';
+import serveStatic from 'serve-static';
+
+const webFolder = './public/';
+
+const app = express();
+const server = config.useHttps ? https.createServer(config.httpsOptions, app) : http.createServer(app);
+
+const mainApp = express();
+const picr22App = express();
+
+app.use(helmet({
+    contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+            "object-src": "'self'", // For SVGs embedded with object tag
+        },
+    }
+}));
+
+mainApp.use(serveStatic(webFolder));
+
+picr22App.use('**', createProxyMiddleware({
+    target: 'http://localhost:8111',
+    changeOrigin: true,
+}));
+
+app.use(vhost('picr22.utr.ee', picr22App));
+app.use(vhost('utr.ee', mainApp));
+
+if (config.useHttps) {
+    const httpApp = express();
+    const httpServer = http.createServer(httpApp);
+
+    httpApp.use(helmet());
+
+    httpApp.use((req, res) => {
+        res.redirect('https://' + req.headers.host + req.url);
+    });
+
+    httpServer.listen(80);
+    server.listen(443);
+} else {
+    server.listen(8100);
+}
